9fccf0f5d3763c099058c02742952b6ee4da10fd
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-13 22:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const para = document.getElementById("status");
 const button = document.getElementById("enterBtn");
 
 button.addEventListener('click',() => {
-	para.outerHTML= `<h1>Entered Metaverse</h1>`;
+	para.innerHTML= `<h1>Entered Metaverse</h1>`;
 })
